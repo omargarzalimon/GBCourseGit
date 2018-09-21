77593cfbf0e70a2f57e8531b6bd1669579e7051a
walterflores4490 <loz.pink.lou@gmail.com>
2018-09-21 08:56:39 -0500

    Paginas anexadas
    Contactos
    Faqs
    Index (incompleto)

---
+++ faqsPage.php
@@ -0,0 +1,135 @@
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/css?family=Barlow" rel="stylesheet">
+<link rel="stylesheet" href="css/styles.css">
+<link rel="stylesheet" href="css/faqsStyle.css">
+
+
+</head>
+<style type="text/css">
+div.midiv {
+    background-color: #e4e4e4;
+}
+div.midiv1{
+  background-color: #D68500;
+}
+
+</style>
+  <body>
+ <!-- Menu -->
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <a class="navbar-brand" href="#">
+      <img src="img/logoGB.png" width="95" height="50">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Library</a>
+        </li>
+        <!-- <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dropdown
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </li> -->
+        <li class="nav-item">
+          <a class="nav-link" href="#">Foroum</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+      <div class="logBtn">
+        <button type="button" class="btn btn-link" data-toggle="modal" data-target="#signModal">My Account</button>
+      </div>
+    </div>
+  </nav>
+  <!--nav end-->
+   <div class="container-fluid">
+  <div class="row">
+    <div class="col-sm-12"><img class="card-img " src="img/header_faqs.png" alt="Card image">
+    </div>
+  </div>
+
+ 
+
+   <div class="row">
+  <div class="col-3 border-right" style="height:70%">
+    <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+      <br>
+      <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">¿How do I get points?</a>
+      <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">¿What are the beercoins?</a>
+      <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">¿Are there personalized advice?</a>
+      <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">¿How do I recover my password?</a>
+    </div>
+  </div>
+  <div class="col-7">
+    <div class="tab-content" id="v-pills-tabContent">
+      <br>
+      <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab "><p class="font-weight-bold faqsTitle">¿How do I get points?</p>
+        <p align="justify">Lorem ipsum dolor sit amet consectetur adipiscing elit, libero scelerisque ullamcorper auctor netus fermentum habitant, justo potenti vivamus aliquam cursus fringilla magnis, varius eget risus class mus bibendum. At suscipit lacus fames tellus cras potenti vulputate sollicitudin nostra, magna taciti enim turpis dui ullamcorper ultricies mauris ut, consequat quisque volutpat pharetra varius mollis class pretium. Sed volutpat purus enim potenti ac tellus habitant ridiculus nunc nec, dignissim vehicula inceptos sollicitudin magnis quam curabitur dapibus conubia tempus lectus, tincidunt leo bibendum dis ad laoreet molestie eros nisl.
+      Faucibus lacinia iaculis ac nascetur nisi massa netus semper, quam eget potenti felis neque placerat fames. Diam vitae lobortis blandit bibendum aliquet nullam faucibus viverra proin, augue aptent volutpat natoque aenean felis a libero, tellus est vivamus non platea ornare ligula feugiat. Tempor pulvinar sed tortor aenean eros, nullam id elementum erat.</p></div>
+      <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab"><p class="font-weight-bold faqsTitle">¿What are the beercoins?</p>
+        <p align="justify">Semper metus aliquam sollicitudin auctor non venenatis nunc lectus erat tristique, orci tellus fringilla nisl consequat pulvinar cras enim fermentum inceptos, laoreet cursus proin vitae fames suspendisse malesuada ante at. Montes lacus fames torquent egestas dictumst feugiat auctor vivamus condimentum ac, penatibus himenaeos mi diam potenti sollicitudin euismod eget ut tortor nisi, aptent arcu accumsan mollis eu est sapien dictum parturient. Urna sapien cum platea class risus litora etiam nascetur potenti quam id primis maecenas, pharetra netus sed integer commodo metus tortor varius dictumst habitant volutpat.</p></div>
+      <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab"><p class="font-weight-bold faqsTitle">¿Are there personalized advice?</p>
+        <p align="justify">Going back to the 1960s, the Lorem Ipsum was popularized by the letterpress manufacturer Letraset, which used it in its advertising campaigns. Letraset offered pages of Lorem Ipsum as transfer sheets, which were widely used in the era before computers for designs. These transfer pages, known as Letraset Body Type, were included in the company's advertising and in its popular catalog.
+
+      The Lorem Ipsum was reintroduced in the 1980s by Aldus Corporation, a company that developed Desktop Publishing Software. Your best-known PageMaker product comes with graphics and pre-installed word processing templates that contain a fake Latin language version.
+      </p></div>
+      <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab"><p class="font-weight-bold faqsTitle">¿How do I recover my password?</p>
+        <p align="justify">This generator uses a Latin word dictionary to build Lorem Ipsum text passages that match your desired length. The duration and punctuation of the sentence and the paragraph are calculated using a Gaussian distribution, based on analysis of real world texts. This ensures that the generated Lorem Ipsum text is unique, free of repetitions and also looks like a legible text as much as possible.
+
+       If you are comfortable with the terminal, you can also install this generator as a command line application. As long as you have Node.js installed, simply run the following command to install it:
+      </p></div>
+    </div>
+  </div>
+</div>
+        
+  </div>
+    
+   <footer class=" mt-3">
+  <div class="container-fluid mx-0 align-items-center">
+    <div class="row text-center">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <img src="img/footer.jpg" class="imgMountains" alt="Image">
+      </div>
+      
+    </div>
+    
+    <div class="row linksRow">
+      <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+        <img src="img/logo_belts_login.png" class="ml-3 imgFooter"  alt="Image">
+        <a class="linkFooter mx-3" href="#">Coach Access</a>
+        <a class="linkFooter mx-3" href="#">What is LSS?</a>
+        <a class="linkFooter mx-3" href="#">FAQs</a>
+        <a class="linkFooter mx-3" href="#">Contact</a>
+        <a class="linkFooter mx-3" href="#">Privacy Policy</a>
+        
+      </div>
+      <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 text-center">
+        <span class="copyright">&#x24B8; 2018 Anheuser-Busch InBev All rights reserved.</span>
+      </div>
+    </div>
+  </div>
+</footer>
+       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,237 @@
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/css?family=Barlow" rel="stylesheet">
+<link rel="stylesheet" href="css/styles.css">
+
+</head>
+<style type="text/css">
+div.midiv {
+    background-color: #913D34;
+}
+div.midiv1{
+  background-color: #D68500;
+}
+
+
+
+</style>
+  <body>
+ <!-- Menu -->
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <a class="navbar-brand" href="#">
+      <img src="img/logoGB.png" width="95" height="50">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Library</a>
+        </li>
+        <!-- <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dropdown
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </li> -->
+        <li class="nav-item">
+          <a class="nav-link" href="#">Foroum</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+      <div class="logBtn">
+        <button type="button" class="btn btn-link" data-toggle="modal" data-target="#loginModal">Log In</button>
+      </div>
+      <div class="logBtn">
+        <button type="button" class="btn btn-link" data-toggle="modal" data-target="#signModal">Sign Up</button>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Menu End -->
+  <!--Carousel--> 
+  <div class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="img/ex1.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="img/ex2.jpg" alt="Second slide"  >
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="img/ex3.jpg" alt="Third slide">
+    </div>
+  </div>
+</div>
+  <!--Carousel End-->
+  <div class="card container-fluid" style="width:100%">
+      <img class="card-img " src="img/slider2home.png" alt="Card image">
+      <div class="card-img-overlay mx-auto  align-items-center" style="width:40%">
+         <div class="mx-auto" style="width:50%" >
+        <h5 class="card-title text-left" style="line-height:75px;color:#64A33D;font-size:50px">Be the best leader!</h5>
+        <p class="text-left" style="line-height:35px;font-size:20px;">Optimize your processes.</p>
+        <p class="text-left" style="line-height:35px;font-size:20px;">Analyze and solve quality problems.</p>
+        <p class="text-left" style="line-height:35px;font-size:20px;">Get better reults.</p>
+
+      </div>
+       
+       </div>
+
+</div>
+  <div class="container-fluid text-center "> <!--Conteiner-->
+
+  <div class="row">
+   <div class="col-sm-12">
+     <img class="img-fluid" src="img/beercoins.jpg">
+   </div> 
+ </div>
+
+  <div class="card-columns">
+  <div class="card">
+    <img class="card-img-top mx-auto" src="img/imgfq.png" alt="Card image cap" style="width:50% " >
+    <div class="card-body">
+         <div class="row">
+            <div class="col-3">
+            <p class="font-weight-bold mb-0" style="font-size:40px;">18</p>
+            <p class="font-weight-bold mb-0" style="font-size:15px;">SEP</p>
+            </div>
+            <div class="col-9">
+              <h5 class="card-title" align="left" style="color:#64A33D;">Card title</h5>
+              <p class="font-weight-bold mb-0" align="left";>subtitle</p>
+              <p class="card-text" align="justify">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top mx-auto" src="img/imgfq.png" alt="Card image cap" style="width:50% ">
+    <div class="card-body">
+        <div class="row">
+            <div class="col-3">
+            <p class="font-weight-bold mb-0" style="font-size:40px;">18</p>
+            <p class="font-weight-bold mb-0" style="font-size:15px;">SEP</p>
+            </div>
+            <div class="col-9">
+              <h5 class="card-title" align="left" style="color:#64A33D;">Card title</h5>
+              <p class="font-weight-bold mb-0" align="left";>subtitle</p>
+              <p class="card-text" align="justify">Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img mx-auto" src="img/imgfq.png" alt="Card image"  style="width:70%">
+    <div class="card-body">
+         <div class="row">
+            <div class="col-3">
+            <p class="font-weight-bold mb-0" style="font-size:40px;">18</p>
+            <p class="font-weight-bold mb-0" style="font-size:15px;">SEP</p>
+            </div>
+            <div class="col-9">
+              <h5 class="card-title" align="left" style="color:#64A33D;">Card title</h5>
+              <p class="font-weight-bold mb-0" align="left";>subtitle</p>
+              <p class="card-text" align="justify">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+    </div>
+  </div>
+   <div class="card">
+    <img class="card-img mx-auto" src="img/imgfq.png" alt="Card image" style="width:70%" >
+    <div class="card-body">
+           <div class="row">
+            <div class="col-3">
+            <p class="font-weight-bold mb-0" style="font-size:40px;">18</p>
+            <p class="font-weight-bold mb-0" style="font-size:15px;">SEP</p>
+            </div>
+            <div class="col-9">
+              <h5 class="card-title" align="left" style="color:#64A33D;">Card title</h5>
+              <p class="font-weight-bold mb-0" align="left";>subtitle</p>
+              <p class="card-text" align="justify">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+    </div>
+  </div>
+   <div class="card">
+    <img class="card-img mx-auto" src="img/imgfq.png" alt="Card image"  style="width:50%">
+    <div class="card-body">
+           <div class="row">
+            <div class="col-3">
+            <p class="font-weight-bold mb-0" style="font-size:40px;">18</p>
+            <p class="font-weight-bold mb-0" style="font-size:15px;">SEP</p>
+            </div>
+            <div class="col-9">
+              <h5 class="card-title" align="left" style="color:#64A33D;">Card title</h5>
+              <p class="font-weight-bold mb-0" align="left";>subtitle</p>
+              <p class="card-text" align="justify">Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+    </div>
+  </div>
+   <div class="card text-center">
+    <img class="card-img mx-auto" src="img/imgfq.png" alt="Card image" style="width:50%">
+    <div class="card-body">
+           <div class="row">
+            <div class="col-3">
+            <p class="font-weight-bold mb-0" style="font-size:40px;">18</p>
+            <p class="font-weight-bold mb-0" style="font-size:15px;">SEP</p>
+            </div>
+            <div class="col-9">
+              <h5 class="card-title" align="left" style="color:#64A33D;">Card title</h5>
+              <p class="font-weight-bold mb-0" align="left";>subtitle</p>
+              <p class="card-text" align="justify">Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+    </div>
+  </div>
+</div>
+ 
+       </div>
+  <!--div conteiner-->
+ <!--footer-->
+  <footer class=" mt-3">
+  <div class="container-fluid mx-0 align-items-center">
+    <div class="row text-center">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <img src="img/footer.jpg" class="imgMountains" alt="Image">
+      </div>
+      
+    </div>
+    
+    <div class="row linksRow">
+      <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+        <img src="img/logo_belts_login.png" class="ml-3 imgFooter"  alt="Image">
+        <a class="linkFooter mx-3" href="#">Coach Access</a>
+        <a class="linkFooter mx-3" href="#">What is LSS?</a>
+        <a class="linkFooter mx-3" href="#">FAQs</a>
+        <a class="linkFooter mx-3" href="contacPage.php">Contact</a>
+        <a class="linkFooter mx-3" href="#">Privacy Policy</a>
+        
+      </div>
+      <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 text-center">
+        <span class="copyright">&#x24B8; 2018 Anheuser-Busch InBev All rights reserved.</span>
+      </div>
+    </div>
+  </div>
+</footer>
+<!--END footer-->
+       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+
+</html>
